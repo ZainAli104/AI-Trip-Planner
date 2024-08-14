ade89f3f60921092c75b2cad932266d9109835e5
ZainAli <chzainali6363@gmail.com>
2024-08-14 12:55:27 +0500

    changes => small fixes

--- src/service/firebaseConfig.js
+++ src/service/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAld36tUPDAJaWjYP1sai0q-Y-gBwRslH4",
-    authDomain: "ai-trip-planner-ed1f8.firebaseapp.com",
-    projectId: "ai-trip-planner-ed1f8",
-    storageBucket: "ai-trip-planner-ed1f8.appspot.com",
-    messagingSenderId: "996729745455",
-    appId: "1:996729745455:web:940f7509a6a789233756da",
-    measurementId: "G-5WSW1SL400"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
